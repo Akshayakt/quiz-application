8864ec0b2b364ce52793c878a436a3d19bc1013b
Midhun <midhun@qburst.com>
2017-08-10 17:25:33 +0530

    quiz component setup

---
+++ src/app/components/core/quiz/questions/questions.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+    selector: 'quiz-questions',
+    templateUrl: 'questions.component.html',
+    styleUrls: ['questions.component.scss']
+})
+
+export class QuestionComponent {
+    constructor() {
+
+    }
+}
--- src/app/components/core/quiz/quiz.module.ts
+++ src/app/components/core/quiz/quiz.module.ts
@@ -1,12 +1,14 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { RouterModule }   from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { QuizComponent }     from './quiz.component';
 
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule
     ],
     declarations: [
         QuizComponent
